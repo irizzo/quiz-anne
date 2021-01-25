3bd99abf11d294481cd28cf6cc01ebb5fc300238
irizzo <isabelle965rizzo@gmail.com>
2021-01-25 13:36:29 -0300

    a

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,55 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizLogo from '../src/components/QuizLogo'
+import Widget from '../src/components/Widget'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `
-
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+      <QuizLogo/>
+        <Widget>
+          <Widget.Header>
+            <h1>The CSS Master</h1>
+          </Widget.Header>
+
+          <Widget.Content>
+            <p>A andiw a qiow mmaoidh a opajwioakniowud </p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Header>
+          <h1>Quizes da Galera</h1>
+          </Widget.Header>
+
+          <Widget.Content>
+            <p>quiz 1</p>
+            <p>quiz 2</p>
+            <p>quiz 3</p>
+          </Widget.Content>
+        </Widget>
+        <Footer/>
+      </QuizContainer>
+      <GitHubCorner projectURL="https://github.com/irizzo/quiz"/>
+    </QuizBackground>
+  )
 }
